46df234ad692682402941d0bc467f375c633832b
Edoardo Gargano <edoardo.gargano@paramount.com>
2023-04-12 09:52:23 +0200

    style in spotify

--- src/features/spotify/Spotify.tsx
+++ src/features/spotify/Spotify.tsx
@@ -1,6 +1,7 @@
 import { getResult, getToken, selectSpotify } from "./spotifySlice";
 import { useEffect } from "react";
 import { useAppSelector, useAppDispatch } from "../../util/hooks";
+import styles from "./Spotify.module.css";
 
 export function Spotify() {
   const { token, spotify } = useAppSelector(selectSpotify);
@@ -12,10 +13,11 @@ export function Spotify() {
 
   return (
     <>
-      <div style={{ backgroundColor: "lightgreen", borderRadius: "15px" }}>
+      <div className={styles.container}>
         <form action="" onSubmit={(e) => e.preventDefault()}>
           <h2>Search artist</h2>
           <input
+            className={styles.input}
             name="searchArtist"
             onChange={(e) => {
               dispatch(
@@ -31,6 +33,7 @@ export function Spotify() {
         <form action="" onSubmit={(e) => e.preventDefault()}>
           <h2>Search song</h2>
           <input
+            className={styles.input}
             name="searchSong"
             onChange={(e) => {
               dispatch(
@@ -44,34 +47,36 @@ export function Spotify() {
           />
         </form>
         <h3>Results</h3>
-        {spotify?.artists &&
-          spotify?.artists?.items?.map((artist) => (
-            <div>
-              <img
-                alt="artist logo"
-                src={artist?.images[0]?.url}
-                style={{ width: "200px" }}
-              ></img>
-              <p>Name: {artist?.name}</p>
-              <p>Genres: {artist?.genres.join(", ")}</p>
-              <p>Followers: {artist?.followers?.total}</p>
-              <p>Popularity: {artist?.popularity}</p>
-            </div>
-          ))}
-        {spotify?.tracks && (
-          <iframe
-            title="spotify"
-            style={{ borderRadius: "12px" }}
-            src={spotify?.tracks?.items[0]?.external_urls?.spotify.replace(
-              "/track",
-              "/embed/track"
-            )}
-            width="100%"
-            height="352"
-            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-            loading="lazy"
-          ></iframe>
-        )}
+        <div className={styles.result}>
+          {spotify?.artists &&
+            spotify?.artists?.items?.map((artist) => (
+              <div className={styles.box}>
+                <img
+                  alt="artist logo"
+                  src={artist?.images[0]?.url}
+                  className={styles.image}
+                ></img>
+                <p>Name: {artist?.name}</p>
+                <p>Genres: {artist?.genres.join(", ")}</p>
+                <p>Followers: {artist?.followers?.total}</p>
+                <p>Popularity: {artist?.popularity}</p>
+              </div>
+            ))}
+          {spotify?.tracks && (
+            <iframe
+              title="spotify"
+              src={spotify?.tracks?.items[0]?.external_urls?.spotify.replace(
+                "/track",
+                "/embed/track"
+              )}
+              width="100%"
+              height="352"
+              allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+              loading="lazy"
+              frameBorder="0"
+            ></iframe>
+          )}
+        </div>
       </div>
     </>
   );
